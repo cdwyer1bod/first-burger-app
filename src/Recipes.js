5a7626ae0fd990ad034254b3e358aca65ab97a75
Gabriel Cooper <gabecoop@hotmail.com>
2021-01-13 16:54:49 +1100

    first commit

--- src/Recipes.js
+++ src/Recipes.js
@@ -7,7 +7,7 @@ const Recipes = ( {item} ) => {
       <h4> {item.meal} </h4>
       <ul>
         {item.ingredients.map(ingredient =>
-          <li key={ingredient.id}> {ingredient.name}: ${twoDP(ingredient.price)} </li>
+          <li key={ingredient.id}> {ingredient.name}: ${twoDP(ingredient.cost)} </li>
         )}
       </ul>
       <SumCost ingredients={item.ingredients} />
@@ -19,7 +19,7 @@ const twoDP = (num) => (Math.round(num * 100) / 100).toFixed(2)
 
 const SumCost = ( {ingredients} ) => {
   const total = ingredients.reduce((accumulator, ingredient) => {
-    return(accumulator + ingredient.price)
+    return(accumulator + ingredient.cost)
   },0)
   return(
     <p> Total cost of ingredients: ${twoDP(total)} </p>
--- src/index.js
+++ src/index.js
@@ -4,29 +4,54 @@ import Recipes from './Recipes'
 
 const twoDP = (num) => (Math.round(num * 100) / 100).toFixed(2)
 
+
+
 const App = () => {
   const [items, setItems] = useState([
     {id: 1, meal: 'Beef Burger', ingredients: [
-        { id: 1, name: 'Cheese', price: 1.50 },
-        { id: 2, name: 'Tomato', price: 1.00 },
-        { id: 3, name: 'Onion', price: 0.75 },
-        { id: 4, name: 'Beef Patty', price: 3.75 }
+        { id: 1, name: 'Cheese', cost: 1.50 },
+        { id: 2, name: 'Tomato', cost: 1.00 },
+        { id: 3, name: 'Onion', cost: 0.75 },
+        { id: 4, name: 'Beef Patty', cost: 3.75 }
       ]
     },
     {id: 2, meal: 'Cheese Burger', ingredients: [
-        { id: 1, name: 'Cheese', price: 1.50 },
-        { id: 2, name: 'Tomato', price: 1.00 },
-        { id: 3, name: 'Onion', price: 0.75 },
-        { id: 4, name: 'Beef Patty', price: 3.75 }
+        { id: 1, name: 'Cheese', cost: 1.50 },
+        { id: 2, name: 'Tomato', cost: 1.00 },
+        { id: 3, name: 'Onion', cost: 0.75 },
+        { id: 4, name: 'Beef Patty', cost: 3.75 }
       ]
     }
   ])
+  const [newItem, setNewItem] = useState('')
 
+	const addItem = (event) => {
+		event.preventDefault()
+		const itemObject = {
+			id: items.length +1,
+			content: newItem,
 
+		}
+		setItems(items.concat(itemObject))
+		setNewItem('')
+	}
+
+	const handleItemChange = (event) => {
+		console.log(event.target.value)
+		setNewItem(event.target.value)
+	}
   return(
     <div>
       <h2> Joe's Burger Joint </h2>
       {items.map(item => <Recipes key={item.id} item={item} />)}
+	  <form onSubmit={addItem}>
+	  	<label>Meal Name</label>
+	  	<input
+			value={newItem}
+			onChange={handleItemChange}
+		/>
+		<button type="submit">Add Meal</button>
+		</form>
     </div>
   )
 }
