9dc1c6aa784fc111808d81a8e38ebfe00e27a930
Chris Dwyer <chrisdwyer@Chriss-MacBook-Pro.local>
2021-01-13 15:28:24 +1100

    Goal 1a

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Meals = ( {item} ) => {
+  console.log({item})
+  return(
+    <div>
+      <h4> {item.meal} </h4>
+      <ul>
+        {item.ingredients.map(ingredient =>
+          <li key={ingredient.id}> {ingredient.name}: ${twoDP(ingredient.price)} </li>
+        )}
+      </ul>
+      <SumCost ingredients={item.ingredients} />
+    </div>
+  )
+}
+
+const twoDP = (num) => (Math.round(num * 100) / 100).toFixed(2)
+
+const SumCost = ( {ingredients} ) => {
+  const total = ingredients.reduce((accumulator, ingredient) => {
+    return(accumulator + ingredient.price)
+  },0)
+  return(
+    <p> Total cost of ingredients: ${twoDP(total)} </p>
+  )
+}
+
 const App = () => {
   const [items, setItems] = useState([
-    { id: 1, ingredient: 'Cheese', price: '$1.50' },
-    { id: 2, ingredient: 'Tomato', price: '$1.00' },
-    { id: 3, ingredient: 'Onion', price: '$0.75' },
-    { id: 4, ingredient: 'Beef Patty', price: '$3.75' }
+    {id: 1, meal: 'Beef Burger', ingredients: [
+        { id: 1, name: 'Cheese', price: 1.50 },
+        { id: 2, name: 'Tomato', price: 1.00 },
+        { id: 3, name: 'Onion', price: 0.75 },
+        { id: 4, name: 'Beef Patty', price: 3.75 }
+      ]
+    }
   ])
 
-  const itemList = items.map(item => <li key={item.id}> {item.ingredient} {item.price} </li>)
 
   return(
     <div>
-      <h2> Joe's Burgers </h2>
-      <div> {itemList} </div>
+      <h2> Joe's Burger Joint </h2>
+      {items.map(item => <Meals key={item.id} item={item} />)}
     </div>
   )
 }
