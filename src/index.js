d93e2e7ec8df47546ce6512893caa3526160b896
Chris Dwyer <chrisdwyer@Chriss-MacBook-Pro.local>
2021-01-13 14:42:41 +1100

    check

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const App = () => {
     { id: 1, ingredient: 'Cheese', price: '$1.50' },
     { id: 2, ingredient: 'Tomato', price: '$1.00' },
     { id: 3, ingredient: 'Onion', price: '$0.75' },
-    { id: 4, ingredient: 'Beef Patty', price: '$3.50' }
+    { id: 4, ingredient: 'Beef Patty', price: '$3.75' }
   ])
 
   const itemList = items.map(item => <li key={item.id}> {item.ingredient} {item.price} </li>)
